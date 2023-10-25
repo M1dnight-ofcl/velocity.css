5d8503bd846387e9c53ddc9afc4b140560f35add
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2023-10-25 16:34:38 -0400

    added config/proper theming

--- velocity/velocity.js
+++
@@ -1,9 +0,0 @@
-const Velocity = class {
-    constructor(defaultThemePath = './themes/default.json') {
-        fetch("./themes/default.json")
-            .then(response => response.json())
-            .then(json => console.log(json));
-    }
-};
-
-const v = Velocity()
