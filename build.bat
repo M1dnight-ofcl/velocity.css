524ab939ce4b48ea2a1600bbd51dafe8a33e3e1b
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2023-10-28 14:32:10 -0400

    updated file builder

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-sass velocity/velocity.func.scss velocity/velocity.func.css
-sass velocity/velocity.style.scss velocity/velocity.style.css
-sass velocity/velocity.full.scss velocity/velocity.full.css
+echo initialized & ^
+sass velocity/velocity.func.scss velocity/velocity.func.css & ^
+sass velocity/velocity.style.scss velocity/velocity.style.css & ^
+sass velocity/velocity.full.scss velocity/velocity.full.css & ^
+echo complete
