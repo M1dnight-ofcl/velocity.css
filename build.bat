a1945e89a9bc3007a9ce5c7053ec455224ec72e0
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2023-10-28 14:28:53 -0400

    created file builder

---
+++ build.bat
@@ -0,0 +1,3 @@
+sass velocity/velocity.func.scss velocity/velocity.func.css
+sass velocity/velocity.style.scss velocity/velocity.style.css
+sass velocity/velocity.full.scss velocity/velocity.full.css
